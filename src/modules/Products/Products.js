c85ad9e85835223ea22b10df34938e19421eb096
cod3f1re <cod4f1rex@gmail.com>
2023-02-23 08:27:59 -0600

    Response, terminado

--- src/modules/Products/Products.js
+++ src/modules/Products/Products.js
@@ -1,22 +1,35 @@
-import React from "react";
-import CardExpandable from "../../library/common/components/cards/CardExpandable";
+import React, {useEffect, useState} from "react";
+import {getRequest} from '../../main/Routes/AxiosIntance';
 
 
 export default function Products() {
+
+    const [post, setPost] = useState([]);
+
+    async function fetchUser() {
+        try {
+            const user = await getRequest('https://jsonplaceholder.typicode.com/users');
+            console.log(user.data)
+            setPost(user?.data);
+        } catch (error) {
+            //Log errors
+        }
+    }
+
+    useEffect(() => {
+        fetchUser()
+    }, []);
+
     return (
         <>
-            <p style={{marginBottom: '30px'}}>
-                Productos
-            </p>
-            <p>
-                <CardExpandable/>
-            </p>
-            <p>
-                <CardExpandable/>
-            </p>
-            <p>
-                <CardExpandable/>
-            </p>
+            Users
+            {post.map((item, i) => {
+                return (
+                    <div key={i}>
+                        <p>{item?.name}</p>
+                    </div>
+                );
+            })}
         </>
-    );
+    )
 }
